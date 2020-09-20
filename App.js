8761f698d797d8d76d0aedd3b40e162318a96040
Henrique Barbosa de Souza Junior <henrique@hbveterinaria.com.br>
2020-09-20 20:44:22 -0300

    UseEffect example

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState} from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+
+import styles from './src/styles/styles'
+
+
 
 export default function App() {
+const [val,setVal] = useState(0);
+const [cont,setCont] = useState(0);
+
+function Contador(){
+  setCont(cont+1);
+}
+
+useEffect(()=>{
+  setVal(val+2);
+},[cont]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.TextLabel}>
+        Exemplo do uso do UseEffect
+      </Text>
       <StatusBar style="auto" />
+
+      <View style={styles.ContainerText}>
+        <Text>utilização do useEffect</Text>
+        <Text>
+          {val}
+        </Text>
+      </View>
+
+      <View style={styles.ContainerText}>
+        <Text>utilização do useState</Text>
+        <Text>
+          {cont}
+        </Text>
+      </View>
+      <TouchableOpacity 
+        style={styles.ButtonCount}
+        onPress={Contador}
+      >
+        <Text>Contador</Text>
+      </TouchableOpacity>
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/styles/styles.js
@@ -0,0 +1,34 @@
+import { StyleSheet } from 'react-native';
+
+const  styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    TextLabel:{
+        fontSize:20,
+
+    },
+    ContainerText:{
+        backgroundColor:'#87CEEB',
+        marginTop:30,
+        height:55,
+        width:'80%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    ButtonCount:{
+        backgroundColor:'#1E90FF',
+        marginTop:30,
+        height:35,
+        borderRadius:7,
+        width:'80%',
+        justifyContent:"center",
+        alignItems:"center"
+    },
+});
+
+export default styles;
+  
